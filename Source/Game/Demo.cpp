9f0dcd030ba54f495f1eb8c95dd47ee7b6be4d4c
minhcongton <minhct@yogame.vn>
2023-03-17 12:25:56 +0700

    Add sample files

---
+++ Source/Game/Demo.cpp
@@ -0,0 +1,23 @@
+﻿#include "Demo.h"
+
+Demo::Demo(const SpawnParams& params)
+    : Script(params)
+{
+    // Enable ticking OnUpdate function
+    _tickUpdate = true;
+}
+
+void Demo::OnEnable()
+{
+    // Here you can add code that needs to be called when script is enabled (eg. register for events)
+}
+
+void Demo::OnDisable()
+{
+    // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+}
+
+void Demo::OnUpdate()
+{
+    // Here you can add code that needs to be called every frame
+}
