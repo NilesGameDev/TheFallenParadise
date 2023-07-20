373082b1e637d3976a6e1cafd5e36f43ed3972f0
minhcongton <minhct@yogame.vn>
2023-07-20 10:18:38 +0700

    Migrate to Flax 1.6 and .net 7

---
+++ Source/FPS/Utilities/Customs/Editors/WeaponEditorWindow.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using FlaxEditor;
+using FlaxEditor.Content;
+using FlaxEditor.CustomEditors;
+using FlaxEditor.CustomEditors.Elements;
+using FlaxEditor.Windows.Assets;
+using FlaxEngine;
+using FPS.Data.Guns;
+
+namespace FPS.Utilities.Customs.Editors
+{
+    /// <summary>
+    /// WeaponEditorWindow Script.
+    /// </summary>
+    public class WeaponEditorWindow : CustomEditorWindow
+    {
+        public override void Initialize(LayoutElementsContainer layout)
+        {
+            layout.Button("Create New Gun");
+            var weaponsDataDir = Path.Combine(Globals.ProjectContentFolder, "Game/Equipments/Weapons/Data");
+
+            var spacer = new SpaceElement();
+            spacer.Spacer.Width = 100;
+            foreach (var file in Directory.EnumerateFiles(weaponsDataDir, "*.json"))
+            {
+                var asset = Content.LoadAsync(file) as JsonAsset;
+                var gun = asset.CreateInstance<Gun>();
+                var assetItem = Editor.Instance.ContentDatabase.Find(asset.ID) as JsonAssetItem;
+                var contentItem = Editor.Instance.ContentDatabase.Find(gun.GunPrefab.ID) as JsonAssetItem;
+                var gunThumbnail = contentItem.Thumbnail;
+
+                var groupElement = layout.Group(gun.Name);
+                var colLayout = new HorizontalPanelElement();
+                var innerRowLayout = new VerticalPanelElement();
+
+                innerRowLayout.Panel.Width = 500;
+                innerRowLayout.Label(gun.Name);
+                var button = new ButtonElement();
+                button.Button.Text = "Modify Gun";
+                button.Button.Clicked += () => new JsonAssetWindow(Editor.Instance, assetItem).Show();
+                innerRowLayout.AddElement(button);
+
+                colLayout.Image(gunThumbnail);
+                colLayout.AddElement(spacer);
+                colLayout.AddElement(innerRowLayout);
+
+                var rowLayout = layout.VerticalPanel();
+                rowLayout.AddElement(colLayout);
+                groupElement.AddElement(rowLayout);
+            }
+        }
+
+        protected override void Deinitialize()
+        {
+            base.Deinitialize();
+            ClearReferenceValueAll();
+        }
+    }
+}
---
+++ Source/FPS/Utilities/Customs/Editors/WeaponManagerScriptEditor.cs
@@ -0,0 +1,27 @@
+﻿using FlaxEditor.CustomEditors;
+using FlaxEditor.CustomEditors.Editors;
+using FlaxEngine;
+using FPS.Combat;
+
+namespace FPS.Utilities.Customs.Editors
+{
+    /// <summary>
+    /// WeaponManagerScriptEditor Script.
+    /// </summary>
+    [CustomEditor(typeof(WeaponManager))]
+    public class WeaponManagerScriptEditor : GenericEditor
+    {
+        public override void Initialize(LayoutElementsContainer layout)
+        {
+            base.Initialize(layout);
+
+            layout.Space(20);
+            var button = layout.Button("Open Weapons Window", Color.YellowGreen);
+            button.Button.Clicked += () =>
+            {
+                var weaponEditor = new WeaponEditorWindow();
+                weaponEditor.Show();
+            };
+        }
+    }
+}
